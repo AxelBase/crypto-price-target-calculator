f1213e24363d1befc9b64a7cf05b535b08724a6e
Sahil Ramjeeth <22046468@dut4life.ac.za>
2025-12-31 18:48:54 +0200

    Fix 1

---
+++ docs/_app/immutable/chunks/B5tXUyme.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1kbc91v?.base??"/crypto-price-target-calculator",t=globalThis.__sveltekit_1kbc91v?.assets??s??"";export{t as a,s as b};
--- docs/_app/immutable/chunks/Bm-RUjnR.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1u2xzjk?.base??"/crypto-price-target-calculator",t=globalThis.__sveltekit_1u2xzjk?.assets??s??"";export{t as a,s as b};
